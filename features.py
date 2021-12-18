9d3d417f2aef62b6bb5270eca285769c3433e507
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2021-12-18 15:12:34 +0100

    Add features.py ver. 0.1a2

--- features.py
+++ features.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 # Name: features
-# Version: 0.1a1
+# Version: 0.1a2
 # Owner: Ruslan Korniichuk
 # Maintainer(s):
 
-from promovolt.readability import word_counter
+from decimal import Decimal
+
+from promovolt.readability import sentence_counter, word_counter
 
 
-def mbrk(sentences, language='en'):
-    """MBRK."""
-    # Filter empty sentences
-    sentences = list(filter(None, sentences))
+def rkmb(text, language='en'):
+    """RKMB."""
+    # Split text to sentences
+    _, sentences_as_text = sentence_counter(text, language)
+
+    # Split sentences to words
+    sentences_as_words = []
+    for sentence in sentences_as_text:
+        _, words = word_counter(sentence, language)
+        # Filter empty sentences
+        if words:
+            sentences_as_words.extend(words)
 
     num = 0
     flag = False
-    for i in range(len(sentences)-1):
-        _, words_current = word_counter(sentences[i], language)
-        _, words_next = word_counter(sentences[i+1], language)
-        if words_current[0] == words_next[0]:
+    for i in range(len(sentences_as_words)-1):
+        if sentences_as_words[i][0] == sentences_as_words[i+1][0]:
             num += 1
             flag = True
-            if i == len(sentences)-2:
+            if i == len(sentences_as_words)-2:
                 num += 1
         else:
             if flag:
                 num += 1
             flag = False
-    mbrk = (num / len(sentences)) * 100
-    return mbrk
+    rkmb = 1 - (Decimal(num) / Decimal(len(sentences_as_words)))
+    rkmb = float(rkmb)
+    return rkmb
