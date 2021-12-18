75d25b7c2eef8fbe0a5e879c4f5d356a2225d574
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2021-12-18 17:19:38 +0100

    Add pus() func to features.py

--- features.py
+++ features.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Name: features
-# Version: 0.1a2
+# Version: 0.1a3
 # Owner: Ruslan Korniichuk
 # Maintainer(s):
 
@@ -9,15 +9,31 @@ from decimal import Decimal
 from promovolt.readability import sentence_counter, word_counter
 
 
-def rkmb(text, language='en'):
+def pus(text, language_code='en'):
+    """PUS -- Percentage of Unique Sentences."""
+    pus = None
+
+    # Filter empty sentences
+    _, sentences = sentence_counter(text, language_code)
+    sentences = list(filter(None, sentences))
+
+    sentences_num = len(sentences)
+    unique_sentences_num = len(set(sentences))
+
+    if sentences_num != 0:
+        pus = unique_sentences_num / sentences_num
+    return pus
+
+
+def rkmb(text, language_code='en'):
     """RKMB."""
     # Split text to sentences
-    _, sentences_as_text = sentence_counter(text, language)
+    _, sentences_as_text = sentence_counter(text, language_code)
 
     # Split sentences to words
     sentences_as_words = []
     for sentence in sentences_as_text:
-        _, words = word_counter(sentence, language)
+        _, words = word_counter(sentence, language_code)
         # Filter empty sentences
         if words:
             sentences_as_words.extend(words)
