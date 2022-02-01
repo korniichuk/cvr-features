f52b2a919f8872cd651714a38cf3e2e0c577256f
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2022-02-01 03:20:46 +0100

    Add apcps() and ppcp() to writer_invariant.py

--- writer_invariant.py
+++ writer_invariant.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # Name: writer_invariant
-# Version: 0.1a2
+# Version: 0.1a3
 # Owner: Ruslan Korniichuk
 # Maintainer(s):
 
 from decimal import Decimal
 
 from promovolt.readability import sentence_counter, word_counter
+
+PCP = ('ADP', 'CCONJ', 'SCONJ', 'PART')
 
 
 def aas(text, nlp, language_code='en'):
@@ -43,6 +45,30 @@ def ans(text, nlp, language_code='en'):
         ans = Decimal(nouns_num) / Decimal(sentences_num)
         ans = float(ans)
     return ans
+
+
+def apcps(text, nlp, language_code='en'):
+    """Average number of Prepositions, Conjunctions, and Particles per Sentence.
+    Docs:
+        ADP -- adposition (e.g., in, to, during)
+        CCONJ -- coordinating conjunction (e.g., and, or, but)
+        SCONJ -- subordinating conjunction (e.g., that, if, while)
+        PART -- particle (e.g., not)
+    """
+
+    apcps = None
+
+    doc = nlp(text)
+
+    sentences_num, _ = sentence_counter(text, language_code)
+
+    pcps = [token.lemma_ for token in doc if token.pos_ in PCP]
+    pcps_num = len(pcps)
+
+    if sentences_num != 0:
+        apcps = Decimal(pcps_num) / Decimal(sentences_num)
+        apcps = float(apcps)
+    return apcps
 
 
 def avs(text, nlp, language_code='en'):
@@ -97,6 +123,30 @@ def pn(text, nlp, language_code='en'):
         pn = Decimal(nouns_num) / Decimal(words_num)
         pn = float(pn)
     return pn
+
+
+def ppcp(text, nlp, language_code='en'):
+    """Percentage of Prepositions, Conjunctions, and Particles in text.
+    Docs:
+        ADP -- adposition (e.g., in, to, during)
+        CCONJ -- coordinating conjunction (e.g., and, or, but)
+        SCONJ -- subordinating conjunction (e.g., that, if, while)
+        PART -- particle (e.g., not)
+    """
+
+    ppcp = None
+
+    doc = nlp(text)
+
+    words_num, _ = word_counter(text, language_code)
+
+    pcps = [token.lemma_ for token in doc if token.pos_ in PCP]
+    pcps_num = len(pcps)
+
+    if words_num != 0:
+        ppcp = Decimal(pcps_num) / Decimal(words_num)
+        ppcp = float(ppcp)
+    return ppcp
 
 
 def pv(text, nlp, language_code='en'):
